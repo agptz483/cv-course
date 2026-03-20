441dc155e73f0a94c47c5b6a2c0b19e56ad75a93
agptz483 <answerstatesall@outlook.com>
2026-03-20 13:42:02 +0800

    加一个显示功能

--- week1/test_cv.py
+++ week1/test_cv.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')  
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
@@ -15,9 +17,15 @@ if __name__ == "__main__":
     print(f" 通道数 (channels)：{channels}")
     print(f" 像素数据类型 (dtype)：{dtype}")
 
+    plt.figure(figsize=(8,8))
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.show()
     cv2.imwrite('Image.jpg', img)
 
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    plt.figure(figsize=(8,8))
+    plt.imshow(cv2.cvtColor(gray_image, cv2.COLOR_BGR2RGB))
+    plt.show()
     cv2.imwrite('gray_example.jpg', gray_image)
 
     p=img[10, 15]
